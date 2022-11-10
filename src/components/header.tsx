e7c0caed25bb01a1f59e7be5f1c9aab346f2f477
Daniel <daniel.fernandez@alumnos.upla.cl>
2022-11-10 20:36:30 -0300

    Configuracion en windows

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
-import '../css/App.css'
+import '../css/app.css'
 
 function Header() {
   return (
-    <div className="Header">
-        <img src={reactLogo} className="App-logo" alt="logo" />
-        <h1>React App</h1>
-        <button onClick={() => console.log('Button Clicked')}>Click Me</button>
-    </div>
+    //Create a header and a navigation bar with links to the home page and the about page with the structure of bootstrap
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={reactLogo} alt="React Logo" width="30" height="24" className="d-inline-block align-text-top" />
+            React App
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
 
